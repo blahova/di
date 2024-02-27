6dbeaa021752813cb644ab65d0cd4a185c0ff305
blahova <118811455+blahova@users.noreply.github.com>
2024-02-27 19:44:51 +0100

    collection+decomposition int (iba simplex)

--- di.R
+++ di.R
@@ -1,77 +1,64 @@
-library(lpSolve)
+library(boot)
 
-collection_integral <- function(f, mi, system)
+collection_integral <- function(f, mi, kolekcia)
 {
-  pocet<-length(f)
-  #prava strana je ta funkcia?
-  #kolekcia bude binarne napr. "101","001" atd
+  pocet<-length(f) #nech viem kolko prvkov je danych
+  funkcia<-c()  #to bude funkcia ktora sa optimalizuje
+  rhs<-f #prava strana
+  dec<-strtoi(kolekcia,2) #kolekcia decimalne
+  matica<-matrix(nrow = pocet, ncol = length(kolekcia)) #inicializacia przdnej matice na simplexku
   
-  
+  for (i in 1:length(kolekcia)) #precadzam prvkami kolekcie
+  {
+    funkcia<-c(funkcia,mi[dec[i]]) #pridam do funkcie hodnotu pre prvok kolekcie
+    for(j in 0:(pocet-1)) #precadzam jednotlive prvky kolekcie po znakoch
+    {
+      if(as.numeric(substr(kolekcia[i],pocet-j,pocet-j))==1)
+      {
+        matica[j+1,i]<-1
+      }
+      else
+      {
+        matica[j+1,i]<-0
+      }
+    }
+  }
+  #vypocitanie
+  vysledok<-simplex(a = funkcia, A1 = matica, b1 = rhs, maxi = TRUE)$value
+  return(unname(vysledok))
+}
+
+decomposition_integral <- function(f, mi, system)
+{
+  vysledky<-c()
   for(i in 1:length(system))
   {
-    funkcia<-c()
-    kolekcia<-system[[i]] #kolekcia ktoru prechadzam
-    rhs<-c(f,rep(0,length(kolekcia)))
-    dir<- c(rep("<=",length(f)),rep(">=",length(kolekcia)))
-    dec<-strtoi(kolekcia,2) #kolekcia decimalne
-    matica <- matrix(nrow = pocet+length(kolekcia), ncol = length(kolekcia))
-    for(j in 1:length(kolekcia))
-    {
-      funkcia<-c(funkcia,mi[dec[j]])
-      for (k in 0:(pocet-1))
-      {
-        if(as.numeric(substr(kolekcia[j],pocet-k,pocet-k))==1)
-        {
-          matica[k+1,j]<-mi[dec[j]]
-          #matica[k+1,j]<-1
-          
-        }
-        else
-        {
-          matica[k+1,j]<-0
-          
-        }
-      }
-      for (k in 1:length(kolekcia))
-      {
-        if(j==k)
-        {
-          matica[k+pocet,j]<-1
-        }
-        else
-        {
-          matica[k+pocet,j]<-0
-        }
-      }
-    }
-    vysledok<-lp("max",funkcia,matica,dir,rhs)
-    print(matica)
-    print(rhs)
-    print(vysledok)
-    print(vysledok$solution)
-    print(dir)
+    vysledky<-c(vysledky,collection_integral(f,mi,system[[i]]))
   }
-
+  
+  maximum<-max(vysledky)
+  poradie<-which.max(vysledky)
+  
+  result <- list(max_value = maximum, coll_num = poradie)
+  class(result) <- "custom_result_class"
+  
+  return(result)
 }
 
-collection_integral(c(1,2,3),c(1,16,25,81,25,100,196),
-                    list(c("111","011","101","001"),c("011","001"),c("001")))
-
-
-
-
-for (i in 1:length(kolekcie)) 
-{
-  for(j in 0:(pocet-1))
-  {
-    #print(as.numeric(substr(kolekcia[i],pocet-j,pocet-j)))
-    if(as.numeric(substr(kolekcie[i],pocet-j,pocet-j))==1)
-    {
-      matica[j+1,i]<-mi[dec[i]]
-    }
-    else
-    {
-      matica[j+1,i]<-0
-    }
-  }
+#overridenuta funkcia na vypis vysledku dekompozicneho integralu
+print.custom_result_class <- function(x) {
+  cat("Max Value:", x$max_value, "\n")
+  cat("Found in collection no.:", x$coll_num, "\n")
 }
+
+f<-c(1,2,3)
+mu<-c(1,16,25,81,25,100,196)
+k1<-c("111","011","101","001")
+k2<-c("011","001")
+k3<-c("001")
+system<-list(k2,k1,k3)
+
+collection_integral(f,mu,k1)
+decomposition_integral(f,mu,system)
+
+
