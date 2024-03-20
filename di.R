b9c7e5956c3da0d377657770621a54cbc38ddb14
blahova <118811455+blahova@users.noreply.github.com>
2024-03-20 10:31:18 +0100

    Add files via upload

--- di.R
+++ di.R
@@ -1,14 +1,14 @@
 library(boot)
 
-Collection <- setRefClass("Collection", 
+CollectionIntegral <- setRefClass("CollectionIntegral", 
                           fields = list(
-                            set_system = "numeric",
+                            set_system = "character",
                             is_chain = "logical",
                             is_single = "logical",
                             is_subpartition = "logical"),
                           methods = list(
                             initialize = function(set_system) {
-                              .self$set_system <- set_system
+                              .self$set_system <- sapply(set_system, make_string, dlzka = max(nchar(set_system)))
                               .self$is_chain <- is_chain(.self$set_system)
                               .self$is_single <- length(.self$set_system) == 1
                               .self$is_subpartition <- is_subpartition(.self$set_system)
@@ -23,12 +23,13 @@ Collection <- setRefClass("Collection",
                           )
 )
 
-Collection$methods(is_chain = function() {
-  if (length(.self$set_system) == 1) {
+
+is_chain = function(set_system) {
+  if (length(set_system) == 1) {
     return(TRUE)
   }
   
-  string_kolekcia <- sapply(.self$set_system, make_string, dlzka = max(nchar(.self$set_system)))
+  string_kolekcia <- sapply(set_system, make_string, dlzka = max(nchar(set_system)))
   ordered <- string_kolekcia[order(strtoi(string_kolekcia, 2))]
   
   pocet <- nchar(ordered[1])
@@ -46,57 +47,52 @@ Collection$methods(is_chain = function() {
   }
   
   return(TRUE)
-})
+}
 
-Collection$methods(is_subpartition = function() {
-  if(length(.self$set_system)==1)
-  {
+is_subpartition = function(set_system) {
+  if(length(set_system) == 1) {
     return(TRUE)
   }
-  velkost<-max(sapply(.self$set_system, function(x) nchar(as.character(x))))
-  string_kolekcia<-sapply(.self$set_system, make_string, dlzka = velkost)
+  
+  velkost <- max(sapply(set_system, function(x) nchar(as.character(x))))
+  string_kolekcia <- sapply(set_system, make_string, dlzka = velkost)
   
   controlSet <- rep(0, velkost)
   
-  for (setIndex in seq_along(string_kolekcia)) 
-  {
+  for (setIndex in seq_along(string_kolekcia)) {
     set <- strsplit(as.character(string_kolekcia[setIndex]), '')[[1]]
-    for (i in seq_along(set)) 
-    {
-      if (set[i] == "1") 
-      {
-        if ("1" %in% unlist(controlSet)[i]) 
-        {
+    for (i in seq_along(set)) {
+      if (set[i] == "1") {
+        if ("1" == unlist(controlSet)[i]) {
           return(FALSE)  
         }
-        controlSet[i]<-"1"
+        controlSet[i] <- "1"
       }
     }
   }
   
   return(TRUE)
-})
+}
 
 
-
-Collection$methods(integral = function(f, mi) {
+CollectionIntegral$methods(compute = function(f, mi) {
   pocet<-length(f) #nech viem kolko prvkov je danych
-  string_kolekcia<-sapply(.self$set_system, make_string, dlzka = pocet)
+  .self$set_system<-sapply(.self$set_system, make_string, dlzka = pocet)
   if(.self$is_single)
   {
-    vysledok <- mi[strtoi(string_kolekcia,2)]
+    vysledok <- mi[strtoi(.self$set_system,2)]
     print("len jeden prvok")
     return(vysledok)
   }
   if(.self$is_chain)
   {
-    vysledok<-chain_int(f,mi,string_kolekcia)
+    vysledok<-chain_int(f,mi,.self$set_system)
     print("je to chain")
     return(vysledok)
   }
   if(.self$is_subpartition)
   {
-    vysledok<-subpartition_int(f,mi,string_kolekcia)
+    vysledok<-subpartition_int(f,mi,.self$set_system)
     print("je to subpartition")
     return(vysledok)
   }
@@ -107,8 +103,8 @@ Collection$methods(integral = function(f, mi) {
   dec<-strtoi(.self$set_system,2) #kolekcia decimalne
   matica <- matrix(0, nrow = pocet, ncol = length(.self$set_system)) #inicializacia przdnej matice na simplexku
   
-  for (i in 1:length(string_kolekcia)) {
-    vector <- as.integer(strsplit(string_kolekcia, "")[[1]]) == 1
+  for (i in 1:length(.self$set_system)) {
+    vector <- as.integer(strsplit(.self$set_system, "")[[1]]) == 1
     matica[, i] <- rev(vector)  
   }
   #vypocitanie
@@ -116,26 +112,8 @@ Collection$methods(integral = function(f, mi) {
   return(unname(vysledok))
 })
 
-
-Decomposition_System <- setRefClass("Decomposition_System", 
-                                    fields = list(
-                                      system = "list"),
-                                    methods = list(
-                                      initialize = function(system) {
-                                        .self$system <- system
-                                      },
-                                      show = function() {
-                                        cat("Dekompozičný Systém: \n")
-                                        for (collection in .self$system) {
-                                          collection$show()
-                                          cat("\n")# Call the show method of each Collection object
-                                        }
-                                      }
-                                    )
-)
-
-Decomposition_System$methods(integral = function(f, mi) {
-  vysledky <- sapply(.self$system, function(s) s$integral(f,mi))
+decomposition_integral = function(f, mi,system) {
+  vysledky <- sapply(system, function(s) s$integral(f, mi))
   
   maximum <- max(vysledky)
   poradie <- which.max(vysledky)
@@ -144,20 +122,13 @@ Decomposition_System$methods(integral = function(f, mi) {
   class(result) <- "custom_result_class"
   print(c("vysledky:", vysledky))
   return(result)
-})
-
-
-system1<-Decomposition_System$new(list(c1,c2,c3))
-system1
-
-
+}
 
 
 find_min<-function(f,mnozina)
 {
   pocet<-length(f)
   rozd<-unlist(strsplit(mnozina,split=""))
-  
   return(min(f[pocet-(which(rozd=="1")-1)]))
 }
 
@@ -168,7 +139,7 @@ chain_int <- function(f, mi, kolekcia) {
   
   vysledok <- find_min(f, ordered[pocet]) * mi[dec[pocet]]
   
-  for (i in seq_len(pocet - 1)) {
+  for (i in (pocet - 1)) {
     vysledok <- vysledok + (find_min(f, ordered[i]) - find_min(f, ordered[i + 1])) * mi[dec[i]]
   }
   
@@ -206,18 +177,22 @@ k3<-c("001")
 system<-list(k2,k1,k3)
 
 
+c1<-CollectionIntegral$new(c(111,011,101,001))
 
-c1<-Collection$new(c(111,011,101,001))
+c1$compute(f,mu)
+
 c2<-Collection$new(c(110,001))
 c3<-Collection$new(c(001))
 
-
 system2<-list(c1,c2,c3)
+decomposition_integral(f,mu,system2)
 
-is_subpartition(c(11001,00100,00011))
+c1$integral(f,mu)
 
-collection_integral(f,mu,c("011","001"))
-decomposition_integral(f,mu,list(k2))
-#' do decomposition interalu MUSI ist list. ak tam pojde len jedna kolekcia tak
-#' to nepojde dobre, lebo bude kazdy prvok brat ako zvlast kolekciu
+system2
+
+
+c1$integral(f,mu)
+c1$integral(f2,mu2)
+
 
